11e408506dd51b557186a4e650231ff8239a9453
Francisco David Sánchez Valencia <franafricano@hotmail.com>
2025-11-22 14:11:31 +0100

    fix(deploy): Set base to empty string for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      base: '/analisissintactico/',
+      base: '',
       define: {
         'process.env.API_KEY': JSON.stringify(process.env.GEMINI_API_KEY || env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(process.env.GEMINI_API_KEY || env.GEMINI_API_KEY)
